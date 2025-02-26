6f9d24e3f346ce637b0d45ef3a5dc7279da5ea70
trinhpvgithub <trinhtrong0505@gmail.com>
2025-02-26 10:22:45 +0700

    UI

--- Commands/StartupCommand.cs
+++ Commands/StartupCommand.cs
@@ -1,5 +1,6 @@
 ﻿using Autodesk.Revit.Attributes;
 using Nice3point.Revit.Toolkit.External;
+using DATN_BeamRebar.View;
 
 namespace DATN_BeamRebar.Commands
 {
@@ -12,6 +13,9 @@ namespace DATN_BeamRebar.Commands
     {
         public override void Execute()
         {
-        }
+            //var view=new View();
+            var view = new DATN_BeamRebar.View.View();
+			view.ShowDialog();
+		}
     }
 }
