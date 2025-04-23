e14f5e22b4d5d51901ffd7d7763d261dbf801d60
TTD-PhamTrinh <p.v.trinh@ttdesignco.com>
2025-04-23 10:22:31 +0700

    update

--- Model/UpperRebar.cs
+++ Model/UpperRebar.cs
@@ -1,58 +1,96 @@
-﻿namespace DATN_BeamRebar.Model;
+﻿using Autodesk.Revit.DB.Structure;
+
+namespace DATN_BeamRebar.Model;
 
 public class UpperRebar
 {
-	private RebarBeamType RebarBeamType { get; set; }
-	private XYZ Start { get; set; }
-	private XYZ End { get; set; }
-	private double Anchor { get; set; }
-	public List<XYZ> Curves { get; set; } = new();
-	public int Quantity { get; set; } = 0;
+    private RebarBeamType RebarBeamType { get; set; }
+    private XYZ Start { get; set; }
+    private XYZ End { get; set; }
+    private double Anchor { get; set; }
+    private List<List<Curve>> Curves { get; set; } = new();
+    private int Quantity { get; set; } = 0;
+    private RebarBarType RebarBarType { get; set; }
+    private Document Document { get; set; }
+    private BeamInfo BeamInfo { get; set; }
 
-	public UpperRebar(RebarBeamType rebarBeamType, XYZ start, XYZ end, double anchor, int quatity)
-	{
-		RebarBeamType = rebarBeamType;
-		Start = start;
-		End = end;
-		Anchor = anchor;
-		Quantity = quatity;
-		RebarAnalys();
-	}
+    public UpperRebar(RebarBeamType rebarBeamType, RebarBarType rebarBarType, BeamInfo beamInfo, double anchor, int quatity)
+    {
+        RebarBeamType = rebarBeamType;
+        BeamInfo = beamInfo;
+        RebarBarType = rebarBarType;
+        Start = BeamInfo.StartPoint;
+        End = BeamInfo.EndPoint;
+        Anchor = anchor;
+        Quantity = quatity;
+        RebarAnalys();
+    }
 
-	private void RebarAnalys()
-	{
-		if (Quantity == 0) return;
-		switch (RebarBeamType)
-		{
-			case RebarBeamType.Top1:
-				Start = Start.Add(50.0.MmToFeet() * -XYZ.BasisZ);
-				End = End.Add(50.0.MmToFeet() * -XYZ.BasisZ);
-				break;
-			case RebarBeamType.Top2:
-				Start = Start.Add(130.0.MmToFeet() * -XYZ.BasisZ);
-				End = End.Add(130.0.MmToFeet() * -XYZ.BasisZ);
-				break;
-			case RebarBeamType.Top3:
-				Start = Start.Add(210.0.MmToFeet() * -XYZ.BasisZ);
-				End = End.Add(210.0.MmToFeet() * -XYZ.BasisZ);
-				break;
-			default:
-				break;
-		}
-		if (Anchor == 0)
-		{
-			Curves.Add(Start);
-			Curves.Add(End);
-		}
-		else
-		{
-			var start = Start.Add(Anchor * -XYZ.BasisZ);
-			var end = End.Add(Anchor * -XYZ.BasisZ);
-			Curves.Add(start);
-			Curves.Add(Start);
-			Curves.Add(End);
-			Curves.Add(end);
-		}
-	}
+    private void RebarAnalys()
+    {
+        if (Quantity == 0) return;
+        switch (RebarBeamType)
+        {
+            case RebarBeamType.Top1:
+                Start = Start.Add(50.0.MmToFeet() * -XYZ.BasisZ);
+                End = End.Add(50.0.MmToFeet() * -XYZ.BasisZ);
+                break;
+            case RebarBeamType.Top2:
+                Start = Start.Add(130.0.MmToFeet() * -XYZ.BasisZ);
+                End = End.Add(130.0.MmToFeet() * -XYZ.BasisZ);
+                break;
+            case RebarBeamType.Top3:
+                Start = Start.Add(210.0.MmToFeet() * -XYZ.BasisZ);
+                End = End.Add(210.0.MmToFeet() * -XYZ.BasisZ);
+                break;
+            default:
+                break;
+        }
+        var width = BeamInfo.Width - 100.0.MmToFeet();
+        var distance = width / (Quantity - 1);
+        Start = Start.Add(width / 2 * -BeamInfo.CrossDirection);
+        End = End.Add(width / 2 * -BeamInfo.CrossDirection);
+        if (Anchor == 0)
+        {
+            for (int i = 0; i < Quantity; i++)
+            {
+                var p1 = Start.Add(i * distance * BeamInfo.CrossDirection);
+                var p2 = End.Add(i * distance * BeamInfo.CrossDirection);
+                List<Curve> curves = new List<Curve>() { Line.CreateBound(p1, p2) };
+                Curves.Add(curves);
+            }
+        }
+        else
+        {
+            var start = Start.Add(Anchor * XYZ.BasisZ);
+            var end = End.Add(Anchor * XYZ.BasisZ);
+            for (int i = 0; i < Quantity; i++)
+            {
+                var p1 = start.Add(i * distance * BeamInfo.CrossDirection);
+                var p2 = Start.Add(i * distance * BeamInfo.CrossDirection);
+                var p3 = End.Add(i * distance * BeamInfo.CrossDirection);
+                var p4 = end.Add(i * distance * BeamInfo.CrossDirection);
+                List<Curve> curves = new List<Curve>() { Line.CreateBound(p1, p2), Line.CreateBound(p2, p3), Line.CreateBound(p3, p4) };
+                Curves.Add(curves);
+            }
+        }
 
+    }
+    public void RebarCreation()
+    {
+        var host = DirectShape.CreateElement(Document, new ElementId(BuiltInCategory.OST_StructuralFraming));
+        foreach (var curves in Curves)
+        {
+
+            try
+            {
+                Document.CreateRebarSingle(RebarStyle.Standard, RebarBarType, host, BeamInfo.CrossDirection, curves);
+
+            }
+            catch (Exception)
+            {
+
+            }
+        }
+    }
 }
